022c59b556b549d385a1a34268bd34d9c32a20ce
Olivier <leplus.olivier@gmail.com>
2017-03-13 16:55:54 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {MdSidenav} from '@angular/material';
+
+import { AlgoliaService } from '@tagazok/algolia-angular-components';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app works!';
+  sortIndices = [];
+
+  @ViewChild(MdSidenav) sidenav: MdSidenav;
+
+  constructor() {
+    
+    this.sortIndices = [
+      {value: 'ikea', label: 'Featured'},
+      {value: 'ikea_price_asc', label: 'Price asc.'},
+      {value: 'ikea_price_desc', label: 'Price desc.'}
+    ];
+  }
+
+  isScreenSmall(): boolean {
+    return window.matchMedia(`(max-width: 840px)`).matches;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '@angular/material';
+import { AlgoliaModule, AlgoliaService } from '@tagazok/algolia-angular-components'
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,9 +16,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AlgoliaModule.forRoot({appId: 'latency', apiKey: '6be0576ff61c053d5f9a3225e2a90f76'}),
+    FlexLayoutModule.forRoot(),
+    MaterialModule.forRoot()
   ],
-  providers: [],
+  providers: [AlgoliaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
